972acbeea3f1d4c5f2e9cbe71dfed58e037e99a3
Ian Henriksen <insertinterestingnamehere@gmail.com>
2015-07-15 17:06:30 -0600

    Fixed C++ exception handling for indexing when it is not used for assignment.

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -3386,6 +3386,10 @@ class IndexNode(ExprNode):
                     func_type = function.type
                     if func_type.is_ptr:
                         func_type = func_type.base_type
+                    self.exception_check = func_type.exception_check
+                    self.exception_value = func_type.exception_value
+                    if func_type.exception_check:
+                        self.is_temp = True
                     self.index = self.index.coerce_to(func_type.args[0].type, env)
                     self.type = func_type.return_type
                     if setting and not func_type.return_type.is_reference:
@@ -3606,7 +3610,6 @@ class IndexNode(ExprNode):
         else:
             if (self.type.is_ptr or self.type.is_array) and self.type == self.base.type:
                 error(self.pos, "Invalid use of pointer slice")
-                return
             index_code = "(%s[%s])"
         return index_code % (self.base.result(), self.index.result())
 
@@ -3701,7 +3704,7 @@ class IndexNode(ExprNode):
                 error_value = '-1'
                 code.globalstate.use_utility_code(
                     UtilityCode.load_cached("GetItemIntByteArray", "StringTools.c"))
-            else:
+            elif not (self.base.type.is_cpp_class and self.exception_check):
                 assert False, "unexpected type %s and base type %s for indexing" % (
                     self.type, self.base.type)
 
@@ -3710,16 +3713,21 @@ class IndexNode(ExprNode):
             else:
                 index_code = self.index.py_result()
 
-            code.putln(
-                "%s = %s(%s, %s%s); if (unlikely(%s == %s)) %s;" % (
-                    self.result(),
-                    function,
-                    self.base.py_result(),
-                    index_code,
-                    self.extra_index_params(code),
-                    self.result(),
-                    error_value,
-                    code.error_goto(self.pos)))
+            if self.base.type.is_cpp_class and self.exception_check:
+                translate_cpp_exception(code, self.pos,
+                    "%s = %s[%s];" % (self.result(), self.base.result(), self.index.result()),
+                    self.exception_value, self.in_nogil_context)
+            else:
+                code.putln(
+                    "%s = %s(%s, %s%s); if (unlikely(%s == %s)) %s;" % (
+                        self.result(),
+                        function,
+                        self.base.py_result(),
+                        index_code,
+                        self.extra_index_params(code),
+                        self.result(),
+                        error_value,
+                        code.error_goto(self.pos)))
             if self.type.is_pyobject:
                 code.put_gotref(self.py_result())
 
